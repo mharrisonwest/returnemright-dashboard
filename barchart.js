a7ed2e92bd6a0084c9095170a564fe850a29b4c1
mharrisonwest <96497322+mharrisonwest@users.noreply.github.com>
2025-09-30 12:32:52 -0400

    chart axis adjustments

--- barchart.js
+++ barchart.js
@@ -38,11 +38,11 @@ r2d3.onRender(function(data, svg, width, height, options) {
   var x = d3.scaleBand()
     .domain(data.map(d => d.group))
     .range([35, innerWidth])
-    .padding(0);
-
+    .padding(.2);
+    
   var y = d3.scaleLinear()
-    .domain([0,data[0].value*1.2]).nice()
-    .range([innerHeight, 0]);
+    .domain([0,data[0].value*.6,data[0].value,data[0].value*1.2])
+    .range([innerHeight,innerHeight*2/3,innerHeight/3,0]);
 
   var color = d3.scaleOrdinal(["#043D5D", "#6FA0A2"]);
 
@@ -54,14 +54,24 @@ r2d3.onRender(function(data, svg, width, height, options) {
     .duration(500)
     .call(d3.axisBottom(x).tickSize(0).tickPadding(15).tickFormat((d,i) => tickLabels[i]))
     .selectAll("text")
-    .style("font-size", "12pt");
+    .style("font-size", "12pt")
+
+  g.select(".x-axis").select("path").remove();
 
   g.select(".y-axis")
     .transition()
     .duration(500)
-    .call(d3.axisLeft(y).ticks(4))
-    .selectAll("text")
-    .style("font-size", "12pt");
+    .call(gAxis => {
+      const maxY = y.domain()[3];
+      gAxis.call(d3.axisLeft(y).ticks(3).tickValues([0, maxY*.5, maxY*.8,maxY]));
+      gAxis.selectAll(".tick line")
+        .attr("x1", -5)
+        .attr("x2", 5)
+        .style("stroke", "#000")
+        .style("stroke-width", 1);
+      gAxis.selectAll("text")
+        .style("font-size", "12pt")
+    });  
 
   var bars = g.selectAll("rect")
     .data(data, d => d.group);
